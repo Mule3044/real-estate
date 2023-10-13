f248f3dc2ce1fd792c202a387b5ef2daf8580606
rich0011 <rich.jdev00@gmail.com>
2023-10-13 11:09:52 +0300

    connect to database

--- api/index.js
+++ api/index.js
@@ -1,4 +1,29 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+dotenv.config();
+
+mongoose.connect(process.env.MONGO).then(() =>{
+    console.log('Conntected to MongoDB');
+})
+.catch((err) =>{
+    console.log(err)
+})
+
+// const path = require('path');
+// require('dotenv').config({
+//     override: true,
+//     path: path.join(__dirname, '.env')
+// });
+// const {Pool, Client} = require('pg');
+
+// const pool = new Pool({
+//     user: process.env.USER,
+//     host: process.env.HOST,
+//     database: process.env.DATABASE,
+//     password: process.env.PASSWORD,
+//     port: process.env.PORT
+// });
 
 const app = express();
 
